f4b7e3def6e3562fbb150f40eacaa6cd2772f9c3
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-04-22 11:25:23 +0200

    fix minor issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :username, presence: true, length: {minimum: 6, maximum: 50}
-  validates :password_digest, presence: true, length: {minimum: 6, maximum: 50}
   validates :name,  presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
